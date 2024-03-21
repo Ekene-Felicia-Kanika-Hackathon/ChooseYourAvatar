da44a2b5e72ecc37b7ce1628ee189eda348a3753
FeliciaNemet <nemet.felicia@gmail.com>
2024-03-21 15:45:29 -0400

    Fixed the blob error

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ START CODE HERE!!!!!! ******/
 
 
 const occupationArr = [
-     {occupation : `chef`,description : `Chefs are culinary magicians who turn ingredients into delicious meals that delight the senses. From gourmet dishes to comfort foods, chefs use their skills and passion to create tasty experiences that bring people together around the dinner table.`, id: 1},
+     {occupation : `chef`,description : `Chefs are culinary magicians who turn ingredients into delicious meals that delight the senses. From gourmet dishes to comfort foods, chefs use their skills and passion to create tasty experiences that bring people together around the dinner table.`},
      {occupation : `astronomer`, description : `Astronomers are cosmic explorers who study the stars, planets, and mysteries of the universe. They use telescopes and scientific tools to unravel the secrets of space, expanding our understanding of the cosmos and our place within it.` },
      {occupation : `doctor`, description : `Doctors are real-life superheroes who help people feel better when they're sick or hurt. They use their knowledge and skills to diagnose illnesses, provide treatment, and promote healthy living for a brighter, happier future.` },
      {occupation : `farmer`, description : `Farmers are nature's caretakers who work hard to grow the food we eat and take care of the land. They nurture plants and animals, harvest crops, and ensure we have fresh, healthy food to nourish our bodies and sustain our communities` },
@@ -32,11 +32,6 @@ const occupationArr = [
      {occupation : `Firefighter`, description : `Firefighters are brave heroes who rush into danger to keep us safe from fires and other emergencies. They work as a team to rescue people, protect property, and extinguish flames, showing courage and selflessness in the face of danger.` },
      {occupation : `Designer`, description : `Designers are creative wizards who bring ideas to life through art, fashion, or digital media. Whether they're crafting beautiful clothes, designing cool websites, or dreaming up imaginative worlds, designers add beauty and style to the world around us.` }
 ];
-
-
-console.log(occupationArr);
-
-// const occupation = `chef`;
 
 
 
@@ -55,16 +50,23 @@ userOccupation();
 
 const occupationPicture = document.querySelector('.picture');
 
-// const getAvatarURL = `https://avatar.iran.liara.run/public/job/${id}/female`;
 
 
-const getOccupation = async (id) => {
+const getOccupation = async (occupation) => {
      try {
-          const occupationResult = await axios.get(`https://avatar.iran.liara.run/public/job/${id}/female`);
+          const occupationResult = await axios.get(`https://avatar.iran.liara.run/public/job/${occupation}/female`, {responseType: "blob"});
 
           console.log(`try block called`);
-          console.log(occupationResult);
-          occupationPicture.src = occupationResult.data;
+          console.log(occupationResult.data);
+          const imageBlob = new Blob([occupationResult.data]);
+          // const imageURL = URL.createObjectURL(new Blob([await occupationResult.data.arrayBuffer],{type:"image/png"}));
+          // console.log(imageURL);
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(imageBlob);
+          fileReader.onloadend = () => {
+               occupationPicture.src = fileReader.result;
+               console.log(fileReader.result);
+          };
 
 
      } catch(error) {
